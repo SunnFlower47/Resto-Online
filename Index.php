a7844f8d987df7e419c1af87102efd03aaf985a7
Ridwan Andrian <ridwanandrian2004@gmail.com>
2025-01-04 08:46:53 +0700

    first commit

--- Index.php
+++ Index.php
@@ -10,7 +10,7 @@ require 'val.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bliss Burger</title>
+    <title>Quick Bite</title>
     <link rel="stylesheet" href="assets/css/style.css" />
     <!-- Font Awesome Icons -->
     <link
